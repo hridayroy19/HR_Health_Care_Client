5622a55f501aa804e5151c164bb8fb20390d2ed8
hridayroy19 <hrhridoyroy503@gmail.com>
2025-09-18 11:11:29 +0600

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import NotFound from "@/components/ui/not-found";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <NotFound
+        particleCount={10000}
+        particleSize={5}
+        animate={true}
+        buttonText="Go Back"
+        buttonHref="/"
+      />
+    </div>
+  );
+}
